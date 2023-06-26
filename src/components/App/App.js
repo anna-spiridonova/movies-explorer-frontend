59bd54705215938dc1e090221ee10990210fa222
Anna Spiridonova <spiridonova.aniutka@yandex.ru>
2023-06-26 21:36:50 +0300

    add header

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import { Route, Routes, useLocation } from "react-router-dom";
+import Header from '../Header/Header';
 
 function App() {
+  const location = useLocation();
+  const isLanding = location.pathname==="/";
+
   return (
     <div className="App">
-      
+      <Header 
+      isLanding={isLanding}
+      />
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,28 @@
+import logo from '../../images/logo.svg';
+import { Route, Routes, NavLink } from 'react-router-dom';
+
+function Header(props) {
+  return (
+    <header className={`header ${props.isLanding ? "header_type_landing" : ""}`}>
+      <NavLink to="/" className="header__logo">
+        <img src={logo} alt="Логотип" />
+      </NavLink>
+      <Routes>
+        <Route path="/" element={
+          <div className="header__container">
+            <NavLink to="/signup" className="header__reg-button">Регистрация</NavLink>
+            <NavLink to="/signin" className="header__log-button">Войти</NavLink>  
+          </div>
+        }/>
+        <Route path="/movies" element={
+          <button>
+          </button>
+        } />
+
+
+      </Routes>
+    </header>
+  );
+}
+
+export default Header;
